7aa98a8a26143c202996a41b299901df72ef6ef7
Kislev Sergey <mr.sergey.kiselev.85@mail.ru>
2025-07-19 10:20:16 +0700

    исправить codestyle

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,11 @@ import usecases.UseCaseOne;
 import usecases.UseCaseSprint6;
 
 public class Main {
-
     public static void main(String[] args) {
         UseCase[] useCases = new UseCase[]{new UseCaseOne(), new UseCaseSprint6()};
-
         for (UseCase usecase : useCases) {
             System.out.println(usecase.getName().toUpperCase());
             usecase.go();
         }
-
     }
 }
