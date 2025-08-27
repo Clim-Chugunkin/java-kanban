7b3ed9e79343f9a7a2d9d5de7afef9276879d69e
Kislev Sergey <mr.sergey.kiselev.85@mail.ru>
2025-08-27 09:38:21 +0700

    спринт 8 исправлен style

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -3,7 +3,6 @@ package manager;
 import exceptions.IntersectedTaskException;
 import exceptions.ManagerSaveException;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
